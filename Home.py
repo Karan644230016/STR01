259d521c0c24c9f673bbaa164aaee29b76e9d5ed
Karan644230016 <96411109+Karan644230016@users.noreply.github.com>
2023-12-15 14:41:41 +0700

    .

--- Home.py
+++ Home.py
@@ -20,18 +20,13 @@ html_1 = """"
         *{
             background-color: #ECF4D6;
         }
-        h5{
-            background-color:#B99470;
-            padding:15px;
-            border-radius:15px 15px 15px 15px;
-            border:2px solid red;
-            text-align:center;
-        }
     </style>
 """
 
 html_2 = """
-<h5>สถิติข้อมูลดอกไม้</h5></center>
+<div style="background-color:#52BE80;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>สถิติข้อมูลดอกไม้</h5></center>
+</div>
 """
 st.markdown(html_1, unsafe_allow_html=True)
 st.markdown("")
@@ -56,7 +51,9 @@ else :
     st.button("Not show bar chart") 
 
 html_3 = """
-    <h5>การทำนายคลาสดอกไม้</h5></center>
+<div style="background-color:#FFBF00;padding:15px;border-radius:15px 15px 15px 15px;border-style:'solid';border-color:black">
+<center><h5>การทำนายคลาสดอกไม้</h5></center>
+</div>
 """
 st.markdown(html_2, unsafe_allow_html=True)
 st.markdown("")   
