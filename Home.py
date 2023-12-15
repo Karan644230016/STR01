efca56fa3e79ed1ec4b76136d0e088eb3d0b2149
Karan644230016 <96411109+Karan644230016@users.noreply.github.com>
2023-12-15 15:32:28 +0700

    .

--- Home.py
+++ Home.py
@@ -1,4 +1,28 @@
+import json
+import time
+import requests
 import streamlit as st
+from streamlit_lottie import st_lottie
+
+def load_lottieurl(url: str):
+    r = requests.get(url)
+    if r.status_code != 200:
+        return None
+    return r.json()
+
+st.header("การวิเคราะห์ความรู้สึกภาษาไทย")
+st.subheader("Kairung Hengpraprohm")
+
+col1, col2 = st.columns(2)
+with col1:
+    st.image('./pic/kairung.jpg')
+    lot3="https://app.lottiefiles.com/share/b48d28ba-de1f-4f6b-a3ef-004121fc987d"
+    lottie3 = load_lottieurl(lot3)
+    st_lottie(lottie3)
+with col2:
+    st.image('./pic/DS1.jpg')
+st.balloons()
+
 
 
 st.header('Karan Khanthong')
