fa44d7a7e16aa92ab49b85e3c2ebae0c2775b86e
Karan644230016 <96411109+Karan644230016@users.noreply.github.com>
2023-12-15 14:25:40 +0700

    .

--- Home.py
+++ Home.py
@@ -11,7 +11,7 @@ col1, col2 = st.columns(2)
 #col1.write("This is column 1")
 #col2.write("This is column 2")
 with col1:
-    st.image('C:./pic/myImg01.jpg')
+    st.image('./pic/myImg01.jpg')
 with col2:
     st.image('./pic/myIris01.jpg')
 
